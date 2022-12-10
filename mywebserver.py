61370815df3fca27d75b8b80ab7ed7e4de5094f9
JEGADEESH07 <jegadeesh942@gmail.com>
2022-12-10 21:59:35 +0530

    Commit the changes

--- mywebserver.py
+++ mywebserver.py
@@ -1,21 +1,3 @@
-# Developing a Simple Webserver
-## AIM:
-To Develop a webserver to display about top five web application development frameworks.
-
-## DESIGN STEPS:
-### Step 1: 
-HTML content creation
-### Step 2:
-Design of webserver workflow
-### Step 3:
-Implementation using Python code
-### Step 4:
-Serving the HTML pages.
-### Step 5:
-Testing the webserver
-
-## PROGRAM:
-```
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 content = """
@@ -43,9 +25,3 @@ server_address = ('', 8080)
 httpd = HTTPServer(server_address, HelloHandler)
 print("my webserver is running.....")
 httpd.serve_forever()
-```
-
-## OUTPUT:
-
-
-## RESULT:
